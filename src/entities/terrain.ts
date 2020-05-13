46beb6718fc640ab31a3e88668a97c97cb4e4d03
Omer Dobzinsky <omer.d@walkme.com>
2020-05-13 15:22:36 +0300

    add bushes

--- src/entities/terrain.ts
+++ src/entities/terrain.ts
@@ -1,20 +1,37 @@
+import type { Position } from '../position';
 import type { EntitySystem } from '../entity-system';
 import { loadImage } from '../core';
+import * as entitySystem from '../entity-system';
 import * as renderingSystem from '../rendering-system';
+import * as positionComponent from '../position-component';
 
 import GrassTileset from '../assets/AH_Autotile_Grass.png';
 import HouseWallTileset from '../assets/AH_Autotile_House_Wall.png';
 import HouseRoofTileset from '../assets/AH_Autotile_House_Roof.png';
+import ObjectsTileset from '../assets/AH_Tileset.png';
 
 const terrainEntityId = 'terrain';
 const tileSize = 16;
+const bushEntityId = (num: number) => `terrain-bush${num}`;
 
 export async function load(es: EntitySystem): Promise<EntitySystem> {;
-    const [grassImage, houseWallImage, houseRoofImage] = await Promise.all([
+    const [grassImage, houseWallImage, houseRoofImage, objectsImage] = await Promise.all([
         loadImage(GrassTileset),
         loadImage(HouseWallTileset),
-        loadImage(HouseRoofTileset)
+        loadImage(HouseRoofTileset),
+        loadImage(ObjectsTileset)
     ]);
+
+    const addBush = (num: number, pos: Position) => [
+        entitySystem.addComponent(positionComponent.positionComponent(bushEntityId(num), pos)),
+        entitySystem.addComponent(renderingSystem.sprite(bushEntityId(num), 1, {
+            image: objectsImage,
+            height: tileSize,
+            width: tileSize,
+            x: 0,
+            y: 5 * tileSize
+        }))
+    ];
 
     return es
         .addComponent(renderingSystem.fromTileset(terrainEntityId, tileSize, 0, [
@@ -63,5 +80,20 @@ export async function load(es: EntitySystem): Promise<EntitySystem> {;
             { image: houseRoofImage, height: tileSize, width: tileSize, x: 8, y: 0 },
             { image: houseRoofImage, height: tileSize, width: tileSize, x: 9, y: 0 },
             { image: houseRoofImage, height: tileSize, width: tileSize, x: 11, y: 0 }
-        ], `\n\n\n\n\n,,,,,,,,,,0,1,1,2`));
+        ], `\n\n\n\n\n,,,,,,,,,,0,1,1,2`))
+        .evolve([
+            ...addBush(1, [3, 3]),
+            ...addBush(2, [100, 30]),
+            ...addBush(3, [150, 60]),
+            ...addBush(4, [97, 100]),
+            ...addBush(5, [250, 40]),
+            ...addBush(6, [260, 45]),
+            ...addBush(7, [120, 200]),
+            ...addBush(8, [130, 210]),
+            ...addBush(9, [200, 250]),
+            ...addBush(10, [260, 260]),
+            ...addBush(11, [240, 200]),
+            ...addBush(12, [30, 270]),
+            ...addBush(13, [42, 267])
+        ]);
 }
